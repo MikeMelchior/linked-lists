f7d903056a177dc51cefd037f01f52fcff9eb370
Mike Melchior <mike.melchior@outlook.com>
2023-03-16 10:34:44 -0400

    add append method to linkedlist class

--- linked-lists-fix.js
+++ linked-lists-fix.js
@@ -2,12 +2,30 @@ class LinkedList {
     constructor () {
         this.head = null;
     }
+
+    append (value) {
+        if (!this.head) {
+            this.head = new Node(value)
+        } else {
+            let tmp = this.head;
+            while (tmp.nextNode) {
+                tmp = tmp.nextNode;
+            }
+            tmp.nextNode = new Node(value);
+        }
+    }
 }
 
 class Node {
-
+    constructor (value = null) {
+        this.value = value;
+        this.nextNode = null;
+    }
 }
 
 
 let list = new LinkedList;
+
+
+
 console.log(list);
